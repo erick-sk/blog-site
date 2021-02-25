acd45f91ef9e4f201b952ef35160d01d12a2f5c2
Erick Saavedra <esvdrah@gmail.com>
2021-02-25 01:22:17 -0500

    Style config from zero erick-sk

--- content/__about/resume-en.md
+++ content/__about/resume-en.md
@@ -1,12 +1,18 @@
 ---
 title: 'about'
-date: 2019-1-27 16:21:13
+date: 2021-1-1 16:21:13
 lang: 'en'
 ---
 
-# Your name
+# Erick Saavedra
 
-<div align="center">
+<div align="justify">
+
+Following my passion of being able to combine hardware and software, I am currently participating in a coding bootcamp in Lima, Peru, which focuses on both the front-end and back-end of programming; the main objective of taking part in this bootcamp is to begin new career as a full-stack developer. Part of my objective is to create fast and scalable solutions which can be maintained through time, focusing on the issue at hand and how to best solve it.
+
+I am not only a reliable and trustworthy person, but I also love to talk and discuss, with a lot of passion, about anything technology-related.
+
+Some of my capabilities and interests include: HTML, CSS, JavaScript, ReactJS, Ruby, Ruby on Rails, PostgreSQL, Git and Github.
 
 _Thank you for reading my resume. If you want to contact me, Please send me an email._
 
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `Erick's Blog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  author: `[Erick Saavedra]`,
+  introduction: `My purpose is to share interesting and relevant information about technology, science and programming.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `ericksk_`, // Your Twitter account
+    github: `erick-sk`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `erick-saavedra-dev`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'erick-sk/comments-blog-gatsby', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
